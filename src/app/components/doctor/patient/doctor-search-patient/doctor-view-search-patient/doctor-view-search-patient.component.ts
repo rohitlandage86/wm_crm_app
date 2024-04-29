70888e7790a49ed415c486020ede7c28c10e7176
Rohitlandage86 <rohitlandage86@gmail.com>
2024-04-29 15:25:53 +0530

    working on prescription

--- src/app/components/doctor/patient/doctor-search-patient/doctor-view-search-patient/doctor-view-search-patient.component.ts
+++ src/app/components/doctor/patient/doctor-search-patient/doctor-view-search-patient/doctor-view-search-patient.component.ts
@@ -573,4 +573,23 @@ export class DoctorViewSearchPatientComponent implements OnInit {
       this.isAccordionOpen = index; // Open the clicked accordion item
     }
   }
+  print(){
+    let printContents, popupWin;
+    printContents = window.document.getElementById('agrrement-section')?.innerHTML
+    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+    if(popupWin){
+      popupWin.document.open();
+      popupWin.document.write(`
+        <html>
+          <head>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+            <body onload="window.print();window.close()">${printContents}</body>
+          </head>
+        </html>
+      `)
+      popupWin.document.close();
+    }
+  }
+
 }
